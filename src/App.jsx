3e77f67719a4c4ff52eea3e66310ea7cce892fcd
qnasir <qnasir575@gmail.com>
2024-01-02 13:35:43 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,28 @@ const imageData = ()=>{
 }
 
 function App() {
-  // code here
+  let img_Arr = imageData();
+return (<div>
+<h1>
+ Kalvium Gallery
+</h1>
+{/* <div className='gallery'>
+<img src={img_Arr[0].img} className='image'/>
+<img src={img_Arr[1].img} className='image'/>
+<img src={img_Arr[2].img} className='image'/>
+<img src={img_Arr[3].img} className='image'/>
+</div> */}
+<div className='parent'>
+{
+  img_Arr.map(elem=>{
+    return(
+    <img src={elem.img} className='image' />
+    )
+  })
+}
+</div>
+</div>
+);
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,47 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
-  // code here
+  render() {
+    let img_Arr = this.imageData();
+    return (
+      <div>
+        <h1>Kalvium Gallery</h1>
+        <div className='gallery'>
+<img src={img_Arr[0].img} className='image'/>
+<img src={img_Arr[1].img} className='image'/>
+<img src={img_Arr[2].img} className='image'/>
+<img src={img_Arr[3].img} className='image'/>
+</div>
+        <div className="parent">
+          {img_Arr.map((elem,i) => {
+            return <img src={elem.img} className="image" key={i}/>;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
